dabaf687e3ea629c76590ecb743ab34e73df36d3
shuhey <shuhey.dev@gmail.com>
2018-10-08 01:11:27 +0900

    プロジェクト ファイルを追加します。

---
+++ WebserviceUwpWrapperTemplate/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace WebserviceUwpWrapperTemplate
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //1．使用したいウェブアプリケーションのURLを指定。http://,https://から指定すること。
+            WebView_Main.Navigate(new Uri("https://www.google.com"));
+        }
+
+        private async void WebView_Main_OnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            //オフラインではなく、指定したページが正常に読み込まれた場合の処理
+            try
+            {
+                //Webページの表示領域にスクロールバーが重なってしまうのを防ぐために以下のJavaScriptを埋め込む。
+                await WebView_Main.InvokeScriptAsync("eval", new string[] { SetScrollbarScript });
+            }
+            catch (Exception e)
+            {
+                WebView_Main.NavigateToString("Offline.");
+            }
+        }
+
+
+        string SetScrollbarScript = @"
+            function setScrollbar()
+            {
+                document.body.style.msOverflowStyle='scrollbar';   
+            } 
+            setScrollbar();";
+    }
+}
+
